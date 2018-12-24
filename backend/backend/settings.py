5d49bfa76823f523d428325ba47e92c849a683d3
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2018-12-24 12:51:46 -0200

    [backend/settings] Allow all HOSTS in settings.py
    Need to set ALLOWED_HOSTS = ['*'] in order to be able to get a
    communication between server and client (react-native app).
    
    Signed-off-by: Rodrigo Rosatti Galvão <rosattig@profusion.mobi>

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'z$$ilp22m1tqenv6@6a_#4!%2nbt!kzk3pgr-m23oxie0u3#u!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
