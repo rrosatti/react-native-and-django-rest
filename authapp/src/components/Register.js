cbb4a246c04c14161b3a013fbd4f73bd25fd6e18
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2018-12-24 11:57:26 -0200

    [client] Change Register to use LoginOrCreateForm
    Signed-off-by: Rodrigo Rosatti Galvão <rosattig@profusion.mobi>

--- authapp/src/components/Register.js
+++ authapp/src/components/Register.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import LoginOrCreateForm from './common/LoginOrCreateForm'
 
 
 class Register extends Component {
   render() {
     return (
-      <View>
-        <Text>Registration screen</Text>
+      <View style={{ flex: 1 }}>
+        <LoginOrCreateForm create/>
       </View>
     );
   }
