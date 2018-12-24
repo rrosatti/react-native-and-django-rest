285c9aca5f978c09a5726baf719816599cbaabc4
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2018-12-24 08:20:19 -0200

    [client] Create Register component
    An initial implementation for Registration was created.
    
    Signed-off-by: Rodrigo Rosatti Galvão <rosattig@profusion.mobi>

---
+++ authapp/src/components/Register.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+class Register extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Registration screen</Text>
+      </View>
+    );
+  }
+}
+
+export default Register;
