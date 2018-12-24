faa543dfb2663cf1150d3679d8b9c79e80f1073e
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2018-12-24 12:12:55 -0200

    [client] Add api (url) location in App.js
    Signed-off-by: Rodrigo Rosatti Galvão <rosattig@profusion.mobi>

--- authapp/App.js
+++ authapp/App.js
@@ -3,6 +3,12 @@ import Router from './src/Router'
 
 
 export default class App extends Component {
+
+  componentWillMount() {
+    axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
+    axios.defaults.timeout = 1500;
+  }
+
   render() {
     return (
       <Router />
