0663b13f3708ab9eb7b61ed5145187606bbaeca5
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2018-12-24 12:49:38 -0200

    [client] Fix missing axios import in App.js

--- authapp/App.js
+++ authapp/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Router from './src/Router'
+import axios from 'axios';
 
 
 export default class App extends Component {
 
   componentWillMount() {
+    # use your local IP address here (the same you use when running the server)
     axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
     axios.defaults.timeout = 1500;
   }
