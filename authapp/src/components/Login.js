3c5f11dd5b0747b4dd52b3bffff3394912b18066
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2018-12-24 11:56:18 -0200

    [client] Change Login to use LoginOrCreateForm
    Signed-off-by: Rodrigo Rosatti Galvão <rosattig@profusion.mobi>

--- authapp/src/components/Login.js
+++ authapp/src/components/Login.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import LoginOrCreateForm from './common/LoginOrCreateForm'
 
 
 class Login extends Component {
   render() {
     return (
-      <View>
-        <Text>Login screen</Text>
+      <View style={{ flex: 1 }}>
+        <LoginOrCreateForm />
       </View>
     );
   }
