a438a8d212b2e7936ad0261976260764096d04bf
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2018-12-24 13:12:09 -0200

    [client] Add Logout functionality
    Add Logout functionality to the Home component.
    
    Signed-off-by: Rodrigo Rosatti Galvão <rosattig@profusion.mobi>

--- authapp/src/components/Home.js
+++ authapp/src/components/Home.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Button, StyleSheet } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import axios from 'axios';
 
 
 class Home extends Component {
+
+  handleRequest() {
+    axios
+      .get('/auth/logout/')
+      .then(response => {
+        Actions.auth()
+      })
+      .catch(error => console.log(error));
+  }
+
   render() {
+    const { buttonContainerStyle } = styles;
     return (
-      <View>
-        <Text>Home screen</Text>
+      <View style={buttonContainerStyle}>
+        <Button title="Logout" onPress={this.handleRequest.bind(this)}/>
       </View>
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  buttonContainerStyle: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: 'white'
+  }
+});
 
 export default Home;
